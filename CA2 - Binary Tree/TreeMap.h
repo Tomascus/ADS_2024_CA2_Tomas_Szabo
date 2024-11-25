a42d3eb3309c9443d536688891488ef50effd73b
Tomas Szabo <bady.tomas33@gmail.com>
2024-11-25 22:40:02 +0000

    ♻️ TreeMap code refactoring
    Reworked the code so the structure is clearer and more intuitive, with methods defined at the start and implemented using templates later on.

--- CA2 - Binary Tree/TreeMap.h
+++ CA2 - Binary Tree/TreeMap.h
@@ -1,6 +1,8 @@
 #include <map>
-#include <utility>
+#include <utility> 
 #include "BinaryTree.h"
+
+using namespace std;
 
 // For now, the tests and treemap contains car names and first letters as key,value for the map - will change for part 2 maybe, also changing it to read from file
 
@@ -8,48 +10,70 @@ template <class K, class V>
 class TreeMap {
 	BinaryTree<pair<K, V>> map; // Im using a binary tree to store key value pairs for the map
 
-	//Implement find node here for get, remove and other methods!!! - instead of calling it in every method
-
+	//Implement find node here for get, remove and other methods!!! - instead of calling it in every method 
 public:
-    void put(K key, V value) 
-    {
-		
-    }
-
-	// Clears the map 
-    void clear() 
-    {
-        map.clear();
-    }
-
-    bool containsKey(K key) 
-    {    
-        return false;
-    }
-
-    V get(K key) 
-    {
-        return V();
-    }
-
-    BinaryTree<K> keySet() 
-    {
-        return BinaryTree<K>();
-    }
-
-	// Returns size of map
-    int size() 
-    {
-        return map.count();
-    }
-
-    bool removeKey(K key) 
-    {
-        return false;
-    }
-
-    V operator[](K key) 
-    {
-        return V();
-    }
+    void clear();
+    bool containsKey(K key);
+    V& get(K key);
+	BinaryTree<K> keySet();
+    void put(K key, V value);
+    int size();
+    bool removeKey(K key);
+    V& operator[](K key);
 };
+
+// Uses clear method from binary tree to clear the map
+template <class K, class V>
+void TreeMap<K, V>::clear() 
+{
+    map.clear();
+}
+
+// This method checks if the key exists in the map
+template <class K, class V>
+bool TreeMap<K, V>::containsKey(K key) 
+{
+    return false;
+}
+
+// This method returns the value of the key in the map
+template <class K, class V>
+V& TreeMap<K, V>::get(K key)
+{
+    //return V();
+}
+
+// This returns a binary tree of keys in the map
+template <class K, class V>
+BinaryTree<K> TreeMap<K, V>::keySet()
+{
+    return BinaryTree<K>();
+}
+
+// This method adds a new pair to the map
+template <class K, class V>
+void TreeMap<K, V>::put(K key, V value) 
+{
+    // CODE HERE LATER
+}
+
+// This method returns the number of pairs in the map
+template <class K, class V>
+int TreeMap<K, V>::size() 
+{
+    return map.count();
+}
+
+// This method removes a key from the map
+template <class K, class V>
+bool TreeMap<K, V>::removeKey(K key) 
+{
+    return false;
+}
+
+// This method returns the value of the key in the map - overloaded operator
+template <class K, class V>
+V& TreeMap<K, V>::operator[](K key) 
+{
+    return V();
+}
