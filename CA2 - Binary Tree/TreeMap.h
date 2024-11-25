30f649d9569635f31a9a59470643f89803ccbbbb
Tomas Szabo <bady.tomas33@gmail.com>
2024-11-25 16:25:04 +0000

    ✨ Size, Clear implementation, Structure fixes
    Removed old test project, added simple methods for checking size based on binary tree code.

--- CA2 - Binary Tree/TreeMap.h
+++ CA2 - Binary Tree/TreeMap.h
@@ -1,49 +1,55 @@
 #include <map>
+#include <utility>
 #include "BinaryTree.h"
 
 // For now, the tests and treemap contains car names and first letters as key,value for the map - will change for part 2 maybe, also changing it to read from file
 
 template <class K, class V>
 class TreeMap {
-private:
-    map<K, V> data;
+	BinaryTree<pair<K, V>> treeMap; // Im using a binary tree to store key value pairs for the map
+
+	//Implement find node here for get, remove and other methods!!! - instead of calling it in every method
+
 public:
-    void put(K key, V value) {
-        
+    void put(K key, V value) 
+    {
 		data[key] = value; // Adds key value pair to map
     }
 
-    void clear() {
-        
+	// Clears the map 
+    void clear() 
+    {
+        tree.clear();
     }
 
-    bool containsKey(K key) {
-        
+    bool containsKey(K key) 
+    {    
         return false;
     }
 
-    V get(K key) {
-        
+    V get(K key) 
+    {
         return V();
     }
 
-    BinaryTree<K> keySet() {
-        
+    BinaryTree<K> keySet() 
+    {
         return BinaryTree<K>();
     }
 
-    int size() {
-        
-        return 0;
+	// Returns size of map
+    int size() 
+    {
+        return tree.count();
     }
 
-    bool removeKey(K key) {
-        
+    bool removeKey(K key) 
+    {
         return false;
     }
 
-    V operator[](K key) {
-        
+    V operator[](K key) 
+    {
         return V();
     }
 };
